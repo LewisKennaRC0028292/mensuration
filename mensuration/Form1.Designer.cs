efd8d24785fb426b8d4b343fb5451c01799d967a
rc0028292 <rc0028292@CRO-ICF07-12>
2017-12-06 11:43:10 +0000

    done dps and sig fig stuff

--- mensuration/Form1.Designer.cs
+++ mensuration/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(384, 130);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(413, 277);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(926, 446);
+            this.Controls.Add(this.richTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +54,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
     }
 }
 
--- mensuration/Form1.cs
+++ mensuration/Form1.cs
@@ -15,11 +15,25 @@ namespace mensuration
         public Form1()
         {
             InitializeComponent();
+            double pi = 3.141592653589793;
+            richTextBox.AppendText(pi.ToString("n3")+ "\n");
+            richTextBox.AppendText(pi.ToString("n5")+"\n");
+            richTextBox.AppendText(pi.ToString("n15") + "\n");
+            double c = 299792458;
+            richTextBox.AppendText(c.ToString("0.0000000") + "\n");
+            richTextBox.AppendText(c.ToString("000E0") + "\n");
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+         
         }
     }
 }
